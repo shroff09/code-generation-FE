cd3ed7de0a621d98e1805760349d7e661a0b4bac
= <=>
2020-09-19 12:32:29 +0530

    created string transform function to format spaces

--- index.test.js
+++ index.test.js
@@ -1,51 +1,57 @@
-const codeGenerator=require("./index");
+const codeGenerator = require("./index");
 
-const removeSpacesFromString=(str)=>{
-    let transformedString="";
-    //Split the string with spaces
-    //If string starts with style prop
-    transformedString+=` ${transformedString[i]}`
-   //else
-    transformedString+=`${transformedString[i]}`
+const removeSpacesFromString = (str) => {
+   let transformedString = "";
+   let retstr = [];
+   //Split the string with spaces
+   //If string starts with style prop
+   retstr = str.split(" ");
 
-    return transformedString;
+   for (var i in retstr) {
+      if (retstr[i].length > 0)
+         if (retstr[i].startsWith('style'))
+            transformedString += ` ${retstr[i]}`
+         else
+            transformedString += `${retstr[i]}`
+   }
+   // console.log(transformedString)
+   return transformedString;
 }
 
 test('Function should return a string', () => {
-    let input={
-        "type":"div",
-        "root":true,
-        "name":"First Section",
-        "style":{
-           "color":"#a8a8a8",
-           "font-size":"32px",
-           "background":"red"
-        },
-        "children":[
-           
-        ]
-     }
-    expect(typeof codeGenerator(input)).toEqual("string");
-  });
+   let input = {
+      "type": "div",
+      "root": true,
+      "name": "First Section",
+      "style": {
+         "color": "#a8a8a8",
+         "font-size": "32px",
+         "background": "red"
+      },
+      "children": [
 
-  test('Node with no child should be self terminating', () => {
-    let input={
-        "type":"div",
-        "root":true,
-        "name":"First Section",
-        "style":{
-           "color":"#a8a8a8",
-           "font-size":"32px",
-           "background":"red"
-        },
-        "children":[
-           
-        ]
-     }
-     let output="<FirstSection/>";
-     let expectedOutput=removeSpacesFromString(output)
-
-     let receivedOutput=removeSpacesFromString(codeGenerator(input))
-     expect(receivedOutput).toEqual(expectedOutput);
+      ]
+   }
+   expect(typeof codeGenerator(input)).toEqual("string");
 });
- 
+
+test('Node with no child should be self terminating', () => {
+   let input = {
+      "type": "div",
+      "root": true,
+      "name": "First Section",
+      "style": {
+         "color": "#a8a8a8",
+         "font-size": "32px",
+         "background": "red"
+      },
+      "children": [
+
+      ]
+   }
+   let output = "<FirstSection style={{'color':'#a8a8a8','fontSize':'32px','background':'red'}}/>";
+   let expectedOutput = removeSpacesFromString(output)
+
+   let receivedOutput = removeSpacesFromString(codeGenerator(input))
+   expect(receivedOutput).toEqual(expectedOutput);
+});
